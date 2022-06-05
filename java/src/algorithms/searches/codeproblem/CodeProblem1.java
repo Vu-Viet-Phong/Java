32668bd0aae75bdc325a367c03b3a6b5a706dd8f
vtenpo <phongzed2001@gmail.com>
2022-06-06 01:46:33 +0700

    commit

--- java/src/algorithms/searches/codeproblem/CodeProblem1.java
+++ java/src/algorithms/searches/codeproblem/CodeProblem1.java
@@ -13,13 +13,46 @@ package algorithms.searches.codeproblem;
  * Output: 4
  */
 public class CodeProblem1 {
-    static int findDisappearedNumbers(int[] arr) {
-        int n = arr.length;
+    static int findDisappearedNumbers1(int[] arr, int n) {
         int sum = ((n + 1) * (n + 2)) / 2;
-
         for (int i = 0; i < n; i++) {
             sum -= arr[i];
         }
         return sum;
+    }
+
+    static int findDisappearedNumbers2(int[] arr, int n) {
+        int total = 1;
+        for (int i = 2; i <= n + 1; i++) {
+            total += i;
+            total -= arr[i - 2];
+        }
+        return total;
+    }
+
+    /** Find missing Number using xor */
+    static int findDisappearedNumbers3(int[] arr, int n) {
+        int x1 = arr[0];
+        int x2 = 1;
+
+        for (int i = 1; i < n; i++) {
+            x1 = x1 ^ arr[i];
+        }
+
+        for (int i = 2; i <= n + 1; i++) {
+            x2 = x2 ^ i;
+        }
+
+        return x1 ^ x2;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5};
+        int n = arr.length;
+
+        System.out.println(findDisappearedNumbers1(arr, n));
+        System.out.println(findDisappearedNumbers2(arr, n));
+        System.out.println(findDisappearedNumbers3(arr, n));
     }
 }
