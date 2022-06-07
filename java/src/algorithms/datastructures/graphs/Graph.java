bb635678c2904f00e115cad38e2a789f5d5f322f
vtenpo <phongzed2001@gmail.com>
2022-06-08 02:00:04 +0700

    commit

--- java/src/algorithms/datastructures/graphs/Graph.java
+++ java/src/algorithms/datastructures/graphs/Graph.java
@@ -1,89 +1,12 @@
 package algorithms.datastructures.graphs;
 
 public class Graph {
-    private static class Vertex {
-        public char label;
-        public boolean wasVisited;
-
-        public Vertex(char label) {
-            this.label = label;
-            wasVisited = false;
-        }
-    }
-
     private final int MAX_VERTS = 20;
-    private int nVerts;          // current number of vertices
-    private int[][] adjMat;      // adjacency matrix
-    private Vertex[] vertexList; // array of vertices
-    private StackX theStack;
+    private int nVerts;
+    private int[][] adjMat;
+    private Vertex[] vertexList;
 
     public Graph() {
-        vertexList = new Vertex[MAX_VERTS];
-                                          // adjacency matrix
-        adjMat = new int[MAX_VERTS][MAX_VERTS];
-        nVerts = 0;
-        for(int y=0; y<MAX_VERTS; y++)      // set adjacency
-            for(int x=0; x<MAX_VERTS; x++)   //    matrix to 0
-                adjMat[x][y] = 0;
-        theStack = new StackX();
-    }
-
-    public void addVertex(char lab) {
-        vertexList[nVerts++] = new Vertex(lab);
-    }
-
-    public void addEdge(int start, int end) {
-        adjMat[start][end] = 1;
-        adjMat[end][start] = 1;
-    }
-
-    public void displayVertex(int v) {
-        System.out.print(vertexList[v].label);
-    }
-
-    /** Depth-first search */
-    public void dfs() {
-        vertexList[0].wasVisited = true;
-        displayVertex(0);
-        theStack.push(0);
-
-        while (!theStack.isEmpty()) {
-            int v = getAdjUnvisitedVertex(theStack.pop());
-            if (v == -1) {
-                theStack.pop();
-            } else {
-                vertexList[v].wasVisited = true;
-                displayVertex(v);
-                theStack.push(v);
-            }
-        }
-    }
-
-    /** Returns an unvisited vertex adj to v */
-    public int getAdjUnvisitedVertex(int v) {
-        for (int j = 0; j < nVerts; j++) {
-            if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
-                return j;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        Graph theGraph = new Graph();
-        theGraph.addVertex('A');    // 0  (start for dfs)
-        theGraph.addVertex('B');    // 1
-        theGraph.addVertex('C');    // 2
-        theGraph.addVertex('D');    // 3
-        theGraph.addVertex('E');    // 4
-
-        theGraph.addEdge(0, 1);     // AB
-        theGraph.addEdge(1, 2);     // BC
-        theGraph.addEdge(0, 3);     // AD
-        theGraph.addEdge(3, 4);     // DE
-
-        System.out.print("Visits: ");
-        theGraph.dfs();             // depth-first search
-        System.out.println();
+        
     }
 }
