d7226d2f8e5c307eaea91385e96368858ce1e133
vtenpo <phongzed2001@gmail.com>
2022-06-08 03:11:51 +0700

    commit

--- java/src/algorithms/datastructures/graphs/Graph.java
+++ java/src/algorithms/datastructures/graphs/Graph.java
@@ -7,6 +7,14 @@ public class Graph {
     private Vertex[] vertexList;
 
     public Graph() {
+        vertexList = new Vertex[MAX_VERTS];
+        adjMat = new int[MAX_VERTS][MAX_VERTS];
+        nVerts = 0;
         
+        for (int row = 0; row < MAX_VERTS; row++) {
+            for (int col = 0; col < MAX_VERTS; col++) {
+                adjMat[row][col] = 0;
+            }
+        }
     }
 }
