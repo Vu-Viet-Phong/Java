3bad65bf13a6ca603ecd22d167033ac7ccd76124
vtenpo <phongzed2001@gmail.com>
2022-06-09 11:52:15 +0700

    commit

--- java/src/algorithms/datastructures/graphs/BFSGraph.java
+++ java/src/algorithms/datastructures/graphs/BFSGraph.java
@@ -108,21 +108,21 @@ public class BFSGraph {
     } 
 
     public static void main(String[] args) {
-        BFSGraph theGraph = new BFSGraph();
+        BFSGraph graph = new BFSGraph();
 
-        theGraph.addVertex('A'); // 0  (start for bfs)
-        theGraph.addVertex('B'); // 1
-        theGraph.addVertex('C'); // 2
-        theGraph.addVertex('D'); // 3
-        theGraph.addVertex('E'); // 4
+        graph.addVertex('A'); // 0  (start for bfs)
+        graph.addVertex('B'); // 1
+        graph.addVertex('C'); // 2
+        graph.addVertex('D'); // 3
+        graph.addVertex('E'); // 4
 
-        theGraph.addEdge(0, 1); // AB
-        theGraph.addEdge(1, 2); // BC
-        theGraph.addEdge(0, 3); // AD
-        theGraph.addEdge(3, 4); // DE
+        graph.addEdge(0, 1); // AB
+        graph.addEdge(1, 2); // BC
+        graph.addEdge(0, 3); // AD
+        graph.addEdge(3, 4); // DE
 
         System.out.print("Visits: ");
-        theGraph.bfs(); // breadth-first search
+        graph.bfs(); // breadth-first search
         System.out.println();
     }
 }
