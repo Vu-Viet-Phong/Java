dc5a32b9d62fac094ad162b27c89a511125ee3f5
vtenpo <phongzed2001@gmail.com>
2022-06-10 11:19:32 +0700

    commit

--- java/src/algorithms/sorts/TopologicalSort.java
+++ java/src/algorithms/sorts/TopologicalSort.java
@@ -37,18 +37,95 @@ public class TopologicalSort {
     }
 
     public void displayVertex(int v) {
-        System.out.println(vertexList[v].label);
+        System.out.print(vertexList[v].label);
     }
 
     public void topo() {
         int origin_nVerts = nVerts;
 
         while (nVerts > 0) {
-            int currentVertex = no
+            int currentVertex = noSuccessors();
+
+            if (currentVertex == -1) {
+                System.out.println("ERROR: Graph has cycles");
+                return;
+            }
+            arr[nVerts - 1] = vertexList[currentVertex].label;
+            deleteVertex(currentVertex);
         }
     }
 
+    /* Returns vert with no successors (or -1 if no such verts) */
     public int noSuccessors() {
         boolean isEdge;
+
+        for (int row = 0; row < nVerts; row++) {
+            isEdge = false;
+
+            for (int col = 0; col < nVerts; col++) {
+                if (adjMatrix[row][col] > 0) {
+                    isEdge = true;
+                    break;
+                }
+            }
+
+            if (!isEdge) {
+                return row;
+            }
+        }
+
+        return -1;
+    }
+
+    public void deleteVertex(int delVert) {
+        if (delVert != nVerts - 1) {
+            for (int i = delVert; i < nVerts - 1; i++) {
+                vertexList[i] = vertexList[i + 1];
+            }
+
+            for (int row = delVert; row < nVerts - 1; row++) {
+                moveRowUp(row, nVerts);
+            }
+
+            for (int col = delVert; col < nVerts - 1; col++) {
+                moveColLeft(col, nVerts - 1);
+            }
+            nVerts--;
+        }
+    }
+
+    private void moveRowUp(int row, int length) {
+        for (int col = 0; col < length; col++) {
+            adjMatrix[row][col] = adjMatrix[row + 1][col];
+        }
+    }
+
+    private void moveColLeft(int col, int length) {
+        for (int row = 0; row < length; row++) {
+            adjMatrix[row][col] = adjMatrix[row][col + 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        TopologicalSort graph = new TopologicalSort();
+        graph.addVertex('A');    // 0
+        graph.addVertex('B');    // 1
+        graph.addVertex('C');    // 2
+        graph.addVertex('D');    // 3
+        graph.addVertex('E');    // 4
+        graph.addVertex('F');    // 5
+        graph.addVertex('G');    // 6
+        graph.addVertex('H');    // 7
+
+        graph.addEdge(0, 3);     // AD
+        graph.addEdge(0, 4);     // AE
+        graph.addEdge(1, 4);     // BE
+        graph.addEdge(2, 5);     // CF
+        graph.addEdge(3, 6);     // DG
+        graph.addEdge(4, 6);     // EG
+        graph.addEdge(5, 7);     // FH
+        graph.addEdge(6, 7);     // GH
+
+        graph.topo(); 
     }
 }
