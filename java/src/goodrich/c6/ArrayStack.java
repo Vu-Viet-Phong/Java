126c54fb17f9e236b2ee2f3ddde1c9c973e236cc
vtenpo <phongzed2001@gmail.com>
2022-07-19 06:47:44 +0700

    Implementation Stack

--- java/src/goodrich/c6/ArrayStack.java
+++ java/src/goodrich/c6/ArrayStack.java
@@ -1,5 +1,9 @@
 package goodrich.c6;
 
+import java.util.Arrays;
+
+import iu.dsa.tung.labs.lab02.Array;
+
 public class ArrayStack<E> implements Stack<E> {
     public static final int CAPACITY = 1000; // default array capacity
     private E[] data; // generic array used for storage
@@ -46,7 +50,21 @@ public class ArrayStack<E> implements Stack<E> {
         return answer;
     }
 
-    public static void main(String[] args) {
+    /** A generic method for reversing an array. */
+    public static <E> void reserve(E[] arr) {
+        int n = arr.length;
+        Stack<E> buffer = new ArrayStack<>(n);
+
+        for (int i = 0; i < n; i++) {
+            buffer.push(arr[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = buffer.pop();
+        }
+    }
+
+    public static void testStack() {
         Stack<Integer> S = new ArrayStack<>(); 
         S.push(5);                          
         S.push(3);                          
@@ -65,5 +83,23 @@ public class ArrayStack<E> implements Stack<E> {
         S.push(6); 
         S.push(8);
         System.out.println(S.pop());
+    }
+
+    public static void testReversingArray() {
+        Integer[] arr = {4, 8, 15, 16, 23, 42}; // autoboxing allows this
+        String[] str = {"Jack", "Kate", "Hurley", "Jin", "Michael"};
+
+        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("str = " + Arrays.toString(str));
+        System.out.println("Reversing...");
+        reserve(arr);
+        reserve(str);
+        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("str = " + Arrays.toString(str));
+    }
+
+    public static void main(String[] args) {
+        // testStack();
+        testReversingArray();
     }
 }
