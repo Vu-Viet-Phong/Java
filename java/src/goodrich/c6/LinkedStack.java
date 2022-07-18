577aef4f7687cfcc3f69a2ed80e2dd66acbe589e
vtenpo <phongzed2001@gmail.com>
2022-07-19 06:38:45 +0700

    Implement Linked Stack

---
+++ java/src/goodrich/c6/LinkedStack.java
@@ -0,0 +1,31 @@
+package goodrich.c6;
+
+import goodrich.c3.SinglyLinkedList;
+
+public class LinkedStack<E> {
+    private SinglyLinkedList<E> list; // an empty list
+
+    public LinkedStack() {
+        list = new SinglyLinkedList<>();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void push(E element) {
+        list.addFirst(element);
+    }
+
+    public E top() {
+        return list.first();
+    }
+
+    public E pop() {
+        return list.removeFirst();
+    }
+}
