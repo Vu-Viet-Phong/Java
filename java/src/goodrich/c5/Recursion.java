455eb9ec8cc60b3754448232edb94ecb14d45cb6
vtenpo <phongzed2001@gmail.com>
2022-07-18 23:15:46 +0700

    comit

---
+++ java/src/goodrich/c5/Recursion.java
@@ -0,0 +1,9 @@
+public class Recursion {
+    public static int factorial(int n) throws IllegalArgumentException {
+        if (n < 0)  {
+            throw new IllegalArgumentException();
+        } else if {
+            return 1;
+        }
+    }
+}
