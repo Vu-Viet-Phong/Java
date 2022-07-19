5d6f7d41090ecc3dd7413a8690948e66a5322d11
vtenpo <phongzed2001@gmail.com>
2022-07-19 07:06:31 +0700

    Demo HTML

---
+++ java/src/goodrich/c6/BracketChecker.java
@@ -0,0 +1,27 @@
+package goodrich.c6;
+
+public class BracketChecker {
+
+    /** Tests if delimiters in the given expression are properly matched. */
+    public static boolean isMatched(String expression) {
+        final String opening = "({["; // opening delimiters
+        final String closing = "]})"; // respective closing delimiters
+        Stack<Character> buffer = new LinkedStack<>();
+
+        for (char c: expression.toCharArray()) {
+            if (opening.indexOf(c) != -1) {
+                buffer.push(c);
+            } else if (closing.indexOf(c) != -1) {
+                if (buffer.isEmpty()) {
+                    return false;
+                }
+
+                if (closing.indexOf(c) != opening.indexOf(buffer.pop())) {
+                    return false;
+                }
+            }
+        }
+
+        return buffer.isEmpty(); // were all opening delimiters matched?
+    }
+}
