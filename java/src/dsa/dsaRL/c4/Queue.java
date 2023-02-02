68ba69bc02fbd812d812eed36f568bf14075c408
vtenpo <phongzed2001@gmail.com>
2023-02-02 11:08:00 +0700

    Queue

---
+++ java/src/dsa/dsaRL/c4/Queue.java
@@ -0,0 +1,100 @@
+package dsa.dsaRL.c4;
+
+/** Implements a Queue using an array */
+public class Queue {
+    private static final int DEFAULT_CAPACITY = 10; 
+    private int maxSize;
+    private long[] queueArray;
+    private int front;
+    private int rear;
+    private int nItems;
+
+    /* Initial Stack with default capacity */
+    public Queue() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /* Initial Stack with input size */
+    public Queue(int size) {
+        maxSize = size;
+        queueArray = new long[maxSize];
+        front = 0;
+        rear = -1;
+        nItems = 0;
+    }
+
+    /** Inserts an element at the rear of the queue */
+    public void insert(long item) {
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
+        queueArray[++rear] = item;
+        nItems++;
+    }
+
+    /**
+     * Remove an element from the front of the queue
+     * @return the new front of the queue
+     */
+    public long remove() {
+        long temp = queueArray[front++];
+
+        if (front == maxSize) {
+            front = 0;
+        }
+        nItems--;
+
+        return temp;
+    }
+
+    /** Return element at the front of the queue */
+    public long peekFront() {
+        return queueArray[front];
+    }
+
+    /** Return element at the rear of the queue */
+    public long peekRear() {
+        return queueArray[rear];
+    }
+
+    /** Returns true if the queue is empty */
+    public boolean isEmpty() {
+        return nItems == 0;
+    }
+
+    /** Returns true if the queue is full */
+    public boolean isFull() {
+        return nItems == maxSize;
+    }
+
+    /** Returns the number of elements in the queue */
+    public int getSize() {
+        return nItems;
+    }
+
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(5);
+
+        /* Insert 4 items */
+        myQueue.insert(10);            
+        myQueue.insert(20);
+        myQueue.insert(30);
+        myQueue.insert(40);
+
+        /* Remove 3 items */
+        myQueue.remove(); // 10 
+        myQueue.remove(); // 20
+        myQueue.remove(); // 30
+
+        /* Insert 4 items */
+        myQueue.insert(50);
+        myQueue.insert(60);
+        myQueue.insert(70);
+        myQueue.insert(80);
+
+        while (!myQueue.isEmpty()) {
+            System.out.print(myQueue.remove() + " ");// (40, 50, 60, 70, 80)
+        }
+        System.out.println("");
+    }
+}
