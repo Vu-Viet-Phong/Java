8e38f39bc182cc547693862e07e01ed9b3600744
vtenpo <phongzed2001@gmail.com>
2023-02-04 20:55:22 +0700

    Selection Sort

--- java/src/dsa/dsaRL/c3/SelectionSort.java
+++ java/src/dsa/dsaRL/c3/SelectionSort.java
@@ -20,7 +20,7 @@ class SelectionSort {
         for (int i = 0; i < n - 1; i++) {
             int min = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min]) {
+                if (arr[min] > arr[j]) {
                     min = j;
                 }
             }
