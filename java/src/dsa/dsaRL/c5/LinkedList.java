00538d94c2c76ca41417c1441f9aa53e50c521ad
vtenpo <phongzed2001@gmail.com>
2023-02-04 13:27:33 +0700

    Linked List

--- java/src/dsa/dsaRL/c5/LinkedList.java
+++ java/src/dsa/dsaRL/c5/LinkedList.java
@@ -1,5 +1,6 @@
 package dsa.dsaRL.c5;
 
+/** Implements a LinkedList */
 public class LinkedList {
     private Link first;
 
@@ -26,8 +27,46 @@ public class LinkedList {
         return temp;
     }
 
+    /** Find link with given key */
+    public Link find(int key) {
+        Link current = first;
+        while (current.data != key) {
+            if (current.next == null) {
+                return null;
+            } else {
+                current = current.next;
+            }
+        }
+        return current;
+    }
+
+    /** Delete link with given key */
+    public Link delete(int key) {
+        Link current = first;
+        Link previous = first;
+
+        while (current.data != key) {
+            if (current.next == null) {
+                return null;
+            } else {
+                previous = current;
+                current = current.next;
+            }
+        }
+
+        if (current == first) {
+            first = first.next;
+        } else {
+            previous.next = current.next;
+        }
+        
+        return current;
+    }
+
     public void displayList() {
-        Link current = first; 
+        Link current = first;
+        
+        System.out.print("List (first-->last): ");
         while (current != null) {
             current.displayLink(); 
             current = current.next;
@@ -50,5 +89,28 @@ public class LinkedList {
             link.displayLink();
             System.out.println("");
         }
+
+        ll.insertFirst(11);
+        ll.insertFirst(33);
+        ll.insertFirst(55);
+        ll.insertFirst(77);
+        ll.insertFirst(99);
+        ll.displayList(); 
+
+        Link f = ll.find(55);
+        if (f != null) {
+            System.out.println("Found link with key " + f.data);
+        } else {
+            System.out.println("Can't find link");
+        }
+
+        Link d = ll.delete(55);
+        if (d != null) {
+            System.out.println("Deleted link with key " + d.data);
+        } else {
+            System.out.println("Can't delete link");
+        }
+
+        ll.displayList(); 
     }
 }
