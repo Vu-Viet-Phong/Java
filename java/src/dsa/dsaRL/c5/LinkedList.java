21697121c99264c63dc6caf39da241eefe48838b
vtenpo <phongzed2001@gmail.com>
2023-02-04 13:07:22 +0700

    Linked List

--- java/src/dsa/dsaRL/c5/LinkedList.java
+++ java/src/dsa/dsaRL/c5/LinkedList.java
@@ -12,11 +12,43 @@ public class LinkedList {
         return (first == null);
     }
 
+    /** Insert at start of list */
     public void insertFirst(int value) {
         Link newLink = new Link(value);
         newLink.next = first;
         first = newLink;
     }
 
+    /** Delete first item */
+    public Link deleteFirst() {
+        Link temp = first;
+        first = first.next;
+        return temp;
+    }
 
+    public void displayList() {
+        Link current = first; 
+        while (current != null) {
+            current.displayLink(); 
+            current = current.next;
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.insertFirst(22);
+        ll.insertFirst(44);
+        ll.insertFirst(66);
+        ll.insertFirst(88);
+        ll.displayList(); 
+
+        while (!ll.isEmpty()) {
+            Link link = ll.deleteFirst();
+            System.out.print("Deleted ");
+            link.displayLink();
+            System.out.println("");
+        }
+    }
 }
